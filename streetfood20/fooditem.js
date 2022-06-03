240032e6bb6d717d80a9c52c4ba475245b60e3ff
AkifCanDUMAN <73740265+AkifCanDUMAN@users.noreply.github.com>
2022-06-04 03:23:40 +0600

    Add files via upload

---
+++ streetfood20/fooditem.js
@@ -0,0 +1,223 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ham Cheese',
+    category : 'pizza',
+    rating : 4.8,
+    price: 15,
+    img: 'images/pizza/hamcheese.png',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Pepperoni Fresh',
+    category : 'pizza',
+    rating : 4.3,
+    price: 14,
+    img: 'images/pizza/pepperonifresh.png',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Mix Pizza',
+    category : 'pizza',
+    rating : 4.1,
+    price: 17,
+    img: 'images/pizza/mix-pizza.png',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Cheese',
+    category : 'pizza',
+    rating : 4.1,
+    price: 13,
+    img: 'images/pizza/Cheese.png',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Caesar salad',
+    category : 'snacks',
+    rating : 4.7,
+    price: 10,
+    img: 'images/snacks/Caesarsalad.png',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Fried Potatoes',
+    category : 'snacks',
+    rating : 4,
+    price: 9,
+    img: 'images/snacks/friedpotatoes.png',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Spicy Tortilla Chicken Wrap',
+    category : 'snacks',
+    rating : 3.5,
+    price: 18,
+    img: 'images/snacks/spicytortillachickenwrap.png',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Tortilla Chicken Wrap',
+    category : 'snacks',
+    rating : 4.5,
+    price: 10,
+    img: 'images/snacks/tortillachickenwrap.png',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Cheese Cake',
+    category : 'desserts',
+    rating : 4.4,
+    price: 10,
+    img: 'images/desserts/Cheesecake.png',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Souffle',
+    category : 'desserts',
+    rating : 4.7,
+    price: 9,
+    img: 'images/desserts/Souffle.png',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Cookies',
+    category : 'desserts',
+    rating : 4.8,
+    price: 8,
+    img: 'images/desserts/Cookies.png',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Macaron Snickers',
+    category : 'desserts',
+    rating : 4.5,
+    price: 7,
+    img: 'images/desserts/MacaronSnickers.png',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Vegan Sushi',
+    category : 'sushi',
+    rating : 4.3,
+    price: 17,
+    img: 'images/sushi/VeganSushi.png',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Banana Sushi',
+    category : 'sushi',
+    rating : 4.8,
+    price: 19,
+    img: 'images/sushi/BananaSushi.png',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Smoked Salmon Avocado Sushi',
+    category : 'sushi',
+    rating : 5,
+    price: 19,
+    img: 'images/sushi/SmokedSalmonAvacadoSushi.png',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'California Roll Sushi',
+    category : 'sushi',
+    rating : 5,
+    price: 22,
+    img: 'images/sushi/CaliforniaRoll.png',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Chicken Burger',
+    category : 'hamburgers',
+    rating : 5,
+    price: 13,
+    img: 'images/hamburgers/ChickenBurger.png',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Double Burger',
+    category : 'hamburgers',
+    rating : 4.9,
+    price: 21,
+    img: 'images/hamburgers/DoubleBurger.png',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Vegan Burger',
+    category : 'hamburgers',
+    rating : 5,
+    price: 20,
+    img: 'images/hamburgers/VeganBurger.png',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Cheese Burger',
+    category : 'hamburgers',
+    rating : 5,
+    price: 12,
+    img: 'images/hamburgers/CheeseBurger.png',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Coke',
+    category : 'drinks',
+    rating : 5,
+    price: 2,
+    img: 'images/drinks/Cola.png',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Fanta',
+    category : 'drinks',
+    rating : 5,
+    price: 4,
+    img: 'images/drinks/Fanta.png',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Sprite',
+    category : 'drinks',
+    rating : 4.5,
+    price: 3,
+    img: 'images/drinks/Sprite.png',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Ice Tea',
+    category : 'drinks',
+    rating : 4.5,
+    price: 6,
+    img: 'images/drinks/IceTea.png',
+    quantity: 1
+},
+]
+
+export {foodItem};
